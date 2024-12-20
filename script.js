17e7785c93a20e9aab81fe87cc2d0043e047b274
tonsison <tonitosison@gmail.com>
2024-12-21 01:07:00 +0800

    Changes Background when Hovering

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ for (i = 0; i < 16; i++) {
   for (j = 0; j < 16; j++) {
     const column = document.createElement("div");
     column.classList.add("column");
-    column.textContent = "X"
+    column.addEventListener('mouseenter', () => {
+      column.style.backgroundColor = "black";
+    });
 
     row.appendChild(column);
   }
